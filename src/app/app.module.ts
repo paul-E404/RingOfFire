0f0419c0e2b9922453257bee978d6c48a4794c36
paul-E404 <paul_engerling@web.de>
2021-05-10 17:15:53 +0200

    Added startscreen design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartscreenComponent } from './startscreen/startscreen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartscreenComponent
   ],
   imports: [
     BrowserModule,
