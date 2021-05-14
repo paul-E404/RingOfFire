f1179faba811467b91ef01aaddc6bdf8b09dd4ab
paul-E404 <paul_engerling@web.de>
2021-05-14 13:41:32 +0200

    Started with implementation of avatar option

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -9,14 +9,22 @@ import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 export class DialogAddPlayerComponent implements OnInit {
 
   name: string = '';
+  avatarNumber: number;
 
   constructor(public dialogRef: MatDialogRef<DialogAddPlayerComponent>) { }
 
   ngOnInit(): void {
+    console.log("avatar Number", this.avatarNumber);
   }
 
   onNoClick() {
     this.dialogRef.close();
   }
+
+  chooseAvatar(i: number) {
+    this.avatarNumber = i;
+    console.log("avatar Number", this.avatarNumber);
+  }
+
 
 }
